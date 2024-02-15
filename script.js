19ceeb25fd5ace38353db782c0ff41ac06110c68
Pejoska99 <biljanapejoska985@gmail.com>
2024-02-15 19:41:03 +0100

    add new js

---
+++ script.js
@@ -0,0 +1,35 @@
+const searchBtn = document.getElementById("searchBtn");
+const inputCountry = document.getElementById("inputCountry");
+
+
+
+async function myCountries() {
+    try{
+    const res = await fetch(`https://restcountries.com/v3.1/all`)
+    const data = await res.json()
+    console.log(data)
+  
+    }
+    catch{(err)=>console.log(err)}
+}
+myCountries()
+
+   
+searchBtn.addEventListener ('click', async function(){
+   
+  const inputCountry = document.getElementById('inputCountry').value;
+    
+try{
+    const countryData = await myCountries(country);
+   
+    console.log(countryData);
+
+        
+}
+catch(error){
+    console.error(error);
+    
+}
+
+   
+});
