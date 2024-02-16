479323d0f5e5908277bbfdf2fd748f855e468c3a
Pejoska99 <biljanapejoska985@gmail.com>
2024-02-16 01:48:24 +0100

    add search countries

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
 const searchBtn = document.getElementById("searchBtn");
+const tBody = document.getElementById("tBody")
 
 
-
-
-async function myCountries() {
+async function fetchCountry(country) {
+    
     try{
-    const res = await fetch(`https://restcountries.com/v3.1/all`)
-    const data = await res.json()
-    console.log(data)
-  
-    }
-    catch{(err)=>console.log(err)}
-}
-myCountries()
+    const response = await fetch(`https://restcountries.com/v3.1/name/${country}`);
+   
+   const data = await response.json();
 
-   
+   return data ;
+}
+catch(error){
+console.error(error);
+throw error;
+}
+
+}
+
+
 searchBtn.addEventListener ('click', async function(){
-   
+  
   const inputCountry = document.getElementById('inputCountry').value;
     
 try{
-    const countryData = await myCountries(country);
-   
-    console.log(countryData);
-
+    const data = await fetchCountry(inputCountry);
+    console.log(data);
+    tBody.innerHTML = '';
+    data.forEach((country) => {            
+    createCountryTable(country);
+       
+});
+}catch(error){
+    console.error(error);
         
 }
-catch(error){
-    console.error(error);
-    
-}
-
    
 });
+  
+  function createCountryTable(country) {
+    tBody.innerHTML += `<tr>
+    <td><img src="${country.flags.png}" width="70" height="40"></td>
+    <td>${country.name.common}</td>
+    <td>${country.population}</td>
+    <td>${country.capital}</td>
+    <td>${country.area}</td>
+    </tr>`;
+  }
+    
+
+
+
+
+
+
