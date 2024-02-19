8e652d86fef6a7d4cf73664ae05a4e13e666b516
Pejoska99 <biljanapejoska985@gmail.com>
2024-02-19 12:24:56 +0100

    new commit css

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const searchBtn = document.getElementById("searchBtn");
-
-
-
 async function fetchCountryByName(country) {
     
     try{
-     
-  
     const response = await fetch(`https://restcountries.com/v3.1/name/${country}`);
    const data = await response.json();
    return data ;
@@ -19,15 +14,12 @@ throw error;
 }
 
 searchBtn.addEventListener ('click', async function(){
-  
-  
-  
+   
   const inputCountry = document.getElementById('inputCountry').value;
    
 try{
     const data = await fetchCountryByName(inputCountry);
     console.log(data);
-    
     
     const tableContainer = document.getElementById("tableContainer");
     tableContainer.innerHTML = '';
@@ -38,8 +30,7 @@ try{
 
 
 }catch(error){
-    console.error(error);
-    
+    console.error(error);  
         
 }
    
@@ -52,13 +43,14 @@ try{
     if (country.languages) {
       languages = Object.values(country.languages).join(", ");
 
-    console.log("lang,",languages)
+    // console.log("lang,",languages)
     if (country.currencies) {
       currencies = Object.values(country.currencies).map(currency => currency.name).join(", ");
   }
-    console.log("curr:",currencies)
+    // console.log("curr:",currencies)
     const googleMapsUrl = country.maps.googleMaps;
-   
+    
+
     tableContainer.innerHTML += `
     <div class="col">
         <div class="card">
@@ -71,6 +63,7 @@ try{
                 <p class="card-text">Languages: ${languages}</p>
                 <p class="card-text">Currencies: ${currencies}</p>
                 <a href="${googleMapsUrl}" target="_blank" class="btn btn-primary">Map</a>
+
             </div>
         </div>
     </div>
@@ -218,6 +211,7 @@ else{
     area: country.area,
     population:country.population
   }));
+  
 }catch(error){
   console.error(error);
 }
